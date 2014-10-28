bb93b94c7a4f68d4e4b1e407bf6aaa7c699e5750
Katherine Fang <kat@firebase.com>
2014-10-28 11:46:05 -0700

    Adding outlet for room view

--- ios/OfficeMover500/OfficeMover500/ViewController.swift
+++ ios/OfficeMover500/OfficeMover500/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var roomView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
