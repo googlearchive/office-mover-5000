682f94e5fe161e4c4ab6fd9a8dcf47e75e402d47
Katherine Fang <kat@firebase.com>
2014-10-28 11:35:30 -0700

    Initial iOS push

---
+++ ios/OfficeMover500/OfficeMover500/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OfficeMover500
+//
+//  Created by Katherine Fang on 10/28/14.
+//  Copyright (c) 2014 Firebase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
