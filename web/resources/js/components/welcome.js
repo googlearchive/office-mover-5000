8305b88a51b2f27aea6149d2667b0d71a3b480b2
Joey Yang <joey@firebase.com>
2014-10-30 13:40:00 -0700

    restructure html and js

--- web/resources/js/components/welcome.js
+++ web/resources/js/components/welcome.js
@@ -7,36 +7,35 @@ var rootRef = new Firebase(utils.urls.root);
 * This is the module that sets up the welcome page and Google login
 */
 
-var $loggedInElements =  $(".toolbar > .toolbar-menu," +
-                           ".toolbar > .toolbar-title," +
-                           ".toolbar > .toolbar-sign-out," +
-                           ".editor");
-
-var $loggedOutElements = $(".buzzwords," +
-                           ".error," +
-                           ".welcome-hero");
+var $welcome = $("#welcome");
+var $app = $("#app");
+var $signInButtons = $(".welcome-hero-signin");
+var $error = $(".error");
 
 var updateUIForLogout = function(){
-  $loggedOutElements.removeClass("hide");
-  $loggedInElements.addClass("hide");
+  $welcome.removeClass("is-hidden");
+  $app.addClass("is-hidden");
 };
 
 var updateUIForLogin = function(){
-  $loggedOutElements.addClass("hide");
-  $loggedInElements.removeClass("hide");
+  $welcome.addClass("is-hidden");
+  $app.removeClass("is-hidden");
 };
 
 var welcome = {
 
   init: function(){
+
     // SETUP LOGIN BUTTON
-    $(".google-signin").on("click", function(e){
-      rootRef.authWithOAuthPopup("google", function(error, authData){
+    $signInButtons.on("click", function(e){
+      var provider = $(this).data("provider");
+
+      rootRef.authWithOAuthPopup(provider, function(error, authData){
         if (error){
-          $(".error").removeClass("error-hide");
+          $error.removeClass("error-hide");
         }
         else {
-          $(".error").addClass("error-hide");
+          $error.addClass("error-hide");
           updateUIForLogin();
         }
       });
@@ -55,28 +54,6 @@ var welcome = {
       else {
         updateUIForLogout(); // USER IS LOGGED OUT
       }
-    });
-
-    // SET LISTENERS ON ADD FURNITURE BUTTONS
-    $(".editor-new").on("click", function(e){
-
-      // MAKE JQUERY OBJECT FOR PIECE OF FURNITURE
-      var itemName = $(this).data("name");          // DESK, PLANT, etc.
-      var $item = $(furnitureTemplates[itemName]);  // jQUERY OBJECT
-      var newItemRef = furnitureRef.push({          // PUSH TO FIREBASE
-        type: itemName,
-        top: 0,
-        left: 0,
-        locked: false,
-        name: "",
-        rotation: 0
-      });
-      var itemID = newItemRef.toString();
-
-      // MAKE DRAGGABLE WITH dragOptions AND APPEND TO DOM
-      $item.data('id', itemID);
-      $item.draggable(dragOptions);
-      $(".editor").append($item);
     });
   }
 };
