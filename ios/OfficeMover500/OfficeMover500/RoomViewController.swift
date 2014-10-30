a84c64a34383c5cadd5e1ec44cb8a66830a5064c
Katherine Fang <kat@firebase.com>
2014-10-29 17:43:59 -0700

    Hiding room setup into a super-view controller.

--- ios/OfficeMover500/OfficeMover500/RoomViewController.swift
+++ ios/OfficeMover500/OfficeMover500/RoomViewController.swift
@@ -1,9 +1,30 @@
 //
-//  RoomViewController.swift
+//  ViewController.swift
 //  OfficeMover500
 //
-//  Created by Katherine Fang on 10/29/14.
+//  Created by Katherine Fang on 10/28/14.
 //  Copyright (c) 2014 Firebase. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+let RoomWidth = 600
+let RoomHeight = 800
+
+class RoomViewController: UIViewController {
+    
+    @IBOutlet weak var roomView: UIView!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        roomView.layer.borderColor = UIColor(red: CGFloat(214.0/255.0), green: CGFloat(235.0/255.0), blue: CGFloat(249.0/255.0), alpha: 1.0).CGColor
+        roomView.layer.borderWidth = 4
+        
+        var nav = self.navigationController?.navigationBar
+        nav?.barTintColor = UIColor(red: CGFloat(22.0/255.0), green: CGFloat(148.0/255.0), blue: CGFloat(223.0/255.0), alpha: 1.0)
+        nav?.barStyle = UIBarStyle.Default
+        nav?.tintColor = UIColor.whiteColor()
+        nav?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+    }
+}
--- ios/OfficeMover500/OfficeMover500/ViewController.swift
+++ ios/OfficeMover500/OfficeMover500/ViewController.swift
@@ -8,12 +8,7 @@
 
 import UIKit
 
-let RoomWidth = 600
-let RoomHeight = 800
-
-class ViewController: UIViewController {
-
-    @IBOutlet weak var roomView: UIView!
+class ViewController: RoomViewController {
     
     let ref = Firebase(url: "https://office-mover.firebaseio.com/")
     let furnitureRef = Firebase(url: "https://office-mover.firebaseio.com/furniture")
@@ -28,20 +23,6 @@ class ViewController: UIViewController {
             self.room.addFurniture(furniture)
             self.createFurnitureView(furniture)
         })
-        
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        roomView.layer.borderColor = UIColor(red: CGFloat(214.0/255.0), green: CGFloat(235.0/255.0), blue: CGFloat(249.0/255.0), alpha: 1.0).CGColor
-        roomView.layer.borderWidth = 4
-        
-        var nav = self.navigationController?.navigationBar
-        nav?.barTintColor = UIColor(red: CGFloat(22.0/255.0), green: CGFloat(148.0/255.0), blue: CGFloat(223.0/255.0), alpha: 1.0)
-        nav?.barStyle = UIBarStyle.Default
-        nav?.tintColor = UIColor.whiteColor()
-        nav?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
     }
     
     // This should take in a Furniture Model whatever that is.
